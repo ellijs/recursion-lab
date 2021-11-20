497b4365c3e704e32e40883f75b77360833b0979
ellijs <ellijs@hotmail.com>
2021-11-20 03:14:42 -0500

    Recursion

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 // Code your solution here!
 
+const printString = (myString) => {
+    console.log(myString[0])
+
+    if ( myString.length > 1 ) {
+        let mySubstring = myString.substring(1, myString.length);
+        printString(mySubstring)
+    } else {
+        return true;
+    }
+}
+
+printString("Hello World!")
